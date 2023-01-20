8366d1a90162e1e88ee964539c638ed7f429b146
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-01-20 08:48:20 +0000

    Username

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,44 @@ import React from 'react'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 /* importing all components*/
+import Username from "./component/Username";
+import Register from "./component/Register";
+import Password from "./component/Password";
+import Reset from "./component/Reset";
+import Recovery from "./component/Recovery";
+import Profile from "./component/Profile";
+import PageNotFound from "./component/PageNotFound";
+
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<div>Root Route</div>
+    element:<Username></Username>
   },
   {
     path:'/register',
-    element:<div>Register Route</div>
-  }
+    element:<Register></Register>
+  },
+  {
+    path:'/password',
+    element:<Password></Password>
+  },
+  {
+    path:'/profile',
+    element:<Profile></Profile>
+  },
+  {
+    path:'/reset',
+    element:<Reset></Reset>
+  },
+  {
+    path:'/recovery',
+    element:<Recovery></Recovery>
+  },
+  {
+    path:'*',
+    element:<PageNotFound></PageNotFound>
+  },
 ])
 
 export default function App() {
--- client/src/component/Username.js
+++ client/src/component/Username.js
@@ -1,7 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Username() {
   return (
-    <div>Username</div>
-  )
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello Again</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Explore more by connecting with us
+            </span>
+          </div>
+        </div>
+
+        <form className="py-1">
+          <div className="profile flex justify-center py-4">
+            <img src="" alt="avatar" />
+          </div>
+          <div className="textbox">
+            <input type="text" placeholder="Username" />
+            <button type="submit">Lets Go</button>
+          </div>
+          <div className="text-center py-4">
+            <span className="text-gray-500">
+              Not a Member
+              <a className="text-red-500" href="/register">
+                Register Now
+              </a>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
