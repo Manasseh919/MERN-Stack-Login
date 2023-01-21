04b781c8c177c3cabde2aa0002f27a1470449227
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-01-21 07:16:37 +0000

    Username UI

--- client/src/component/Username.js
+++ client/src/component/Username.js
@@ -1,35 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png"
+import styles from '../styles/Username.module.css'
+
+
 export default function Username() {
   return (
     <div className="container mx-auto">
-      <div className="flex justify-center items-center h-screen">
-        <div>
+      <div className="flex  justify-center items-center h-screen">
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold">Hello Again</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Explore more by connecting with us
             </span>
           </div>
-        </div>
+        
 
         <form className="py-1">
           <div className="profile flex justify-center py-4">
-            <img src="" alt="avatar" />
+            <img className={styles.profile_img} src={avatar} alt="avatar" />
           </div>
-          <div className="textbox">
-            <input type="text" placeholder="Username" />
-            <button type="submit">Lets Go</button>
+          <div className="flex flex-col items-center py-4 textbox">
+            <input type="text" className={styles.textbox} placeholder="Username" />
+            <button type="submit" className={styles.btn}>Lets Go</button>
           </div>
           <div className="text-center py-4">
             <span className="text-gray-500">
-              Not a Member
-              <a className="text-red-500" href="/register">
+              Not a Member {" "}
+              <Link className="text-red-500" to="/register">
                 Register Now
-              </a>
+              </Link>
             </span>
           </div>
         </form>
+        </div>
       </div>
     </div>
   );
